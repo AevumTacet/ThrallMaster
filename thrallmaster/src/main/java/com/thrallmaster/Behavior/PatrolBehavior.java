faea7ef3a2b587d8e5e774a48bd9162f8563c32b
JotaRata <jjbarria.c@gmail.com>
2025-08-15 05:59:56 +0000

    Implemented logic for PatrolBehavior

--- thrallmaster/src/main/java/com/thrallmaster/Behavior/PatrolBehavior.java
+++ thrallmaster/src/main/java/com/thrallmaster/Behavior/PatrolBehavior.java
@@ -3,22 +3,27 @@ package com.thrallmaster.Behavior;
 import java.util.UUID;
 
 import org.bukkit.Location;
+import org.bukkit.entity.AbstractSkeleton;
+import org.bukkit.entity.Player;
 
+import com.thrallmaster.AggressionState;
+import com.thrallmaster.Settings;
 import com.thrallmaster.States.ThrallState;
+import com.thrallmaster.Utils.BehaviorUtils;
 
 public class PatrolBehavior extends Behavior {
 	public static int WAIT_TIME = 5;
 	private int index;
 	private Location startLocation;
-	private Location EndLocation;
-	private double timer;
+	private Location endLocation;
+	private double startTime;
+	private int elapsedTicks;
 
 	public PatrolBehavior(UUID entityID, ThrallState state, Location starLocation, Location endLocation) {
 		super(entityID, state);
 		this.index = 0;
 		this.startLocation = starLocation;
-		this.EndLocation = endLocation;
-		this.timer = 0;
+		this.endLocation = endLocation;
 	}
 
 	@Override
@@ -28,14 +33,56 @@ public class PatrolBehavior extends Behavior {
 
 	@Override
 	public void onBehaviorStart() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'onBehaviorStart'");
+		var entity = this.getEntity();
+
+		if (entity != null) {
+			entity.setTarget(null);
+		}
+		this.startTime = 0;
 	}
 
 	@Override
 	public void onBehaviorTick() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'onBehaviorTick'");
+		AbstractSkeleton entity = this.getEntity();
+		if (entity == null) {
+			return;
+		}
+
+		Location target = this.index == 0 ? startLocation : endLocation;
+		double distance = BehaviorUtils.distance(entity, target);
+
+		if (distance > Settings.THRALL_FOLLOW_MIN) {
+			entity.getPathfinder().moveTo(target);
+		} else {
+			if (elapsedTicks % 100 == 0) {
+				BehaviorUtils.randomWalk(entity, startLocation);
+			}
+			startTime = System.currentTimeMillis();
+		}
+
+		if (elapsedTicks % 10 == 0) {
+			if (state.aggressionState == AggressionState.HOSTILE) {
+				BehaviorUtils.findClosestEnemy(state, this);
+			} else if (state.aggressionState == AggressionState.HEALER) {
+				BehaviorUtils.findClosestAlly(state, this);
+			}
+		}
+
+		if (System.currentTimeMillis() - startTime > WAIT_TIME * 1000) {
+			index = index == 0 ? 1 : 0;
+		}
+
+		Player owner = state.getOwner();
+		if (owner != null) {
+			double distancePlayer = BehaviorUtils.distance(entity, owner.getLocation());
+			if (distancePlayer < Settings.THRALL_FOLLOW_MIN / 2) {
+				entity.getPathfinder().stopPathfinding();
+				entity.lookAt(state.getOwner().getEyeLocation());
+				return;
+			}
+		}
+
+		elapsedTicks++;
 	}
 
 }
